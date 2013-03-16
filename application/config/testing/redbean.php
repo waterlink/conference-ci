30697b62824cc0746a59c397fb3939a62c312fbe
waterlink000 (Alexey F.) <waterlink000@gmail.com>
2013-03-16 23:32:25 +0200

    trying to shoot error

--- application/config/testing/redbean.php
+++ application/config/testing/redbean.php
@@ -5,7 +5,12 @@ $config["redbean_auth"] = false;
 // R::setup($config['redbean_connection_string']);
 if(file_exists("/home/dotcloud/environment.json")) {
     /* configuration on dotCloud */
-    require('env.php');
+    $env =  json_decode(file_get_contents("/home/dotcloud/environment.json"));
+	$user = $env->DOTCLOUD_DB_MYSQL_LOGIN;
+	$password = $env->DOTCLOUD_DB_MYSQL_PASSWORD; 
+	$host = $env->DOTCLOUD_DB_MYSQL_HOST;
+	$port = $env->DOTCLOUD_DB_MYSQL_PORT;
+	$dbname = 'test';
     
     $config["redbean_connection_string"] = "mysql:dbname=$dbname;host=$host;port=$port";
     $config["redbean_auth"] = true;
--- application/models/rinit.php
+++ application/models/rinit.php
@@ -5,9 +5,13 @@ class Rinit extends CI_Model {
 		global $argv;
 		if ($argv[1] != 'test'){
 			if ($this->config->item('redbean_auth')){
-				R::setup($this->config->item('redbean_connection_string'),
-					$this->config->item('redbean_user'),
-					$this->config->item('redbean_password'));
+				try{
+					R::setup($this->config->item('redbean_connection_string'),
+						$this->config->item('redbean_user'),
+						$this->config->item('redbean_password'));
+				} catch (Exception $e) {
+					echo 'Caught exception: ',  $e->getMessage(), "\n";
+				}
 			} else {
 				R::setup($this->config->item('redbean_connection_string'));
 			}
