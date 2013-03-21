496038bf7fd8b3f7262b0161389cd462fd84bfef
waterlink000 (Alexey F.) <waterlink000@gmail.com>
2013-03-21 03:16:21 +0200

    files are placed to the right place on server

--- application/controllers/uploads.php
+++ application/controllers/uploads.php
@@ -7,11 +7,24 @@ class Uploads extends CI_Controller {
 		$this->load->model('User');
 		$this->load->model('Auth');
 	}
-	public function index(){
-		$this->load->library('UploadHandler');
+	public function index($id = false){
+		if (!$id){
+			$this->load->library('UploadHandler');
+		} else {
+			$this->load->library('UploadHandler', array(
+				'upload_dir' => dirname($_SERVER['SCRIPT_FILENAME']).'/files/'.$id.'/',
+			));
+		}
 	}
-	public function info(){
-		echo phpinfo();
+	// public function info(){
+	// 	echo phpinfo();
+	// }
+	public function id(){
+		$bean = R::dispense('upload');
+		R::store($bean);
+		echo json_encode(array(
+			'id' => $bean->id
+		));
 	}
 
 }
